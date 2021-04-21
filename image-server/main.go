e96fc69ce6c8f2c1a3bfc127965891c2d6dddabc
s1ntaxe770r <cyberdev01@protonmail.com>
2021-04-21 13:58:14 +0100

    setup static fileserver

--- image-server/main.go
+++ image-server/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"image-server/handlers"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-
+	router.StaticFS("/images", http.Dir("./images"))
 	router.POST("/upload", handlers.Upload())
 
 	router.Run(":8000")
