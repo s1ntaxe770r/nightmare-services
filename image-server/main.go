2611eeae342503133db9b3b679bd086d643eb016
s1ntaxe770r <cyberdev01@protonmail.com>
2021-04-23 01:03:55 +0100

    allow cors

--- image-server/main.go
+++ image-server/main.go
@@ -4,11 +4,13 @@ import (
 	"image-server/handlers"
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.Default())
 	router.StaticFS("/images", http.Dir("./images"))
 	router.POST("/upload", handlers.Upload())
 
