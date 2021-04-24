fbd4c9c05d17ca6e08a6888484f40f3acd821f8a
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-04-24 22:31:45 +0100

    move index -> templates

--- gallery/app.py
+++ gallery/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask,url_for
 from flask.templating import render_template
 from flask_cors import CORS
 from os import getenv
-import requests 
+import requests
+
 
 
 
@@ -25,6 +26,9 @@ def index():
         image_urls.append(url)
     return render_template("index.html",image_urls=image_urls)
 
-
+@app.route("/upload")
+def upload():
+    upload_route = image_server+"/upload"
+    return render_template("upload.html",upload_route=upload_route)
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=8080)
+    app.run('0.0.0.0',port=8080,debug=True)
