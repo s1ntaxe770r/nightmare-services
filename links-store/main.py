4761e9508d4e1fb182604563976b07c2f7d3ea46
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-06-07 15:12:48 +0100

    move utils because i have a life to live
    Signed-off-by: Jubril Oyetunji <cyberdev01@protonmail.com>

--- links-store/main.py
+++ links-store/main.py
@@ -2,16 +2,61 @@ from os import link
 from flask import Flask, jsonify, Response
 from flask.globals import request
 from flask_cors import CORS
-from .utils import snip
 import json
 
 app = Flask(__name__)
 CORS(app)
 
+
+
+
+
+import redis 
+from os import getenv
+from typing import List
+
+
+host:str =  getenv("REDIS_HOST")
+port:str =  getenv("REDIS_PORT")
+
+
+
+
+class Link:
+    """
+    Link class represents an image link 
+    """
+    def __init__(self,url:str) -> None:
+        self.url = url
+        
+
+
+
+def add_link(link:Link) -> Link:
+    """
+    insert a new link into the cache 
+    Input : Link
+    Returns: Link
+    """
+    client = redis.Redis(host,port,0)
+    client.rpush('links',link.url)
+    return link.url
+
+
+def get_links() -> List[bytes]:
+    """
+    Retrive all the links from the cache
+    Returns: List
+    """
+    client = redis.Redis(host,port,0)
+    links = client.lrange('links',0,-1)
+    return links 
+
+
 @app.route("/links/get", methods=["GET"])
 def retrive_links():
     response = Response()
-    bytelist = snip.get_links()
+    bytelist = get_links()
     results = []
     for link in bytelist:
         string = link.decode()
@@ -25,8 +70,8 @@ def insert_link():
     response = Response()
     request_data = request.json
     image_name = request_data['image_name']
-    link = snip.Link(image_name)
-    return_val = snip.add_link(link)
+    link = Link(image_name)
+    return_val = add_link(link)
     return jsonify({'stored': return_val})
 
 
